37a985e3874d3fde24f83562891bfaa81265ed93
Alexis G <contact@g-alexis.com>
2021-05-07 14:19:54 +0900

    quick fix

--- model/categoryManager.php
+++ model/categoryManager.php
@@ -1,53 +1,56 @@
 <?php
 
-    // require("manager.php");
+// require("manager.php");
 
-    function categoriesInfoModel () {
-        $dataBase = dbConnect();
-        $rawResponse = $dataBase->query("SELECT name FROM categories");
-        $infoArray = $rawResponse->fetchAll(PDO::FETCH_ASSOC);
-        $rawResponse->closeCursor();
-        return $infoArray;
-    }
+function categoriesInfoModel()
+{
+    $dataBase = dbConnect();
+    $rawResponse = $dataBase->query("SELECT name FROM categories");
+    $infoArray = $rawResponse->fetchAll(PDO::FETCH_ASSOC);
+    $rawResponse->closeCursor();
+    return $infoArray;
+}
 
-    function defaultSearch() {
-        $dataBase = dbConnect();
-        $rawResponse = $dataBase->query(
-            "SELECT c.name AS categoryName, e.name AS eventName, e.eventDate as eventDate, e.playerNumber as playerNumber, e.duration as duration, c.image as categoryImage 
+function defaultSearch()
+{
+    $dataBase = dbConnect();
+    $rawResponse = $dataBase->query(
+        "SELECT c.name AS categoryName, e.name AS eventName, e.eventDate as eventDate, e.playerNumber as playerNumber, e.duration as duration, c.image as categoryImage 
             FROM events e
-            OUTER JOIN categories c
+            JOIN categories c
             ON e.categoryId = c.id"
-        );
-        $infoArray = $rawResponse->fetchAll(PDO::FETCH_ASSOC);  
-        $rawResponse->closeCursor();
-        return $infoArray;
-    }
+    );
+    $infoArray = $rawResponse->fetchAll(PDO::FETCH_ASSOC);
+    $rawResponse->closeCursor();
+    return $infoArray;
+}
 
-    function inputSearch($name) {
-        $dataBase = dbConnect();
-        $rawResponse = $dataBase->query(
-            "SELECT c.name AS categoryName, e.name AS eventName, e.eventDate as eventDate, e.playerNumber as playerNumber, e.duration as duration, c.image as categoryImage 
+function inputSearch($name)
+{
+    $dataBase = dbConnect();
+    $rawResponse = $dataBase->query(
+        "SELECT c.name AS categoryName, e.name AS eventName, e.eventDate as eventDate, e.playerNumber as playerNumber, e.duration as duration, c.image as categoryImage 
             FROM events e
             JOIN categories c
             ON e.categoryId = c.id
             WHERE e.name = '$name'"
-        );
-        $infoArray = $rawResponse->fetchAll(PDO::FETCH_ASSOC);  
-        $rawResponse->closeCursor();
-        return $infoArray;
-    }
+    );
+    $infoArray = $rawResponse->fetchAll(PDO::FETCH_ASSOC);
+    $rawResponse->closeCursor();
+    return $infoArray;
+}
 
-    function selectSearch($sportName) {
-        $dataBase = dbConnect();
-        $rawResponse = $dataBase->query(
-            "SELECT c.name AS categoryName, e.name AS eventName, e.eventDate as eventDate, e.playerNumber as playerNumber, e.duration as duration, c.image as categoryImage 
+function selectSearch($sportName)
+{
+    $dataBase = dbConnect();
+    $rawResponse = $dataBase->query(
+        "SELECT c.name AS categoryName, e.name AS eventName, e.eventDate as eventDate, e.playerNumber as playerNumber, e.duration as duration, c.image as categoryImage 
             FROM events e
             JOIN categories c
             ON e.categoryId = c.id
             WHERE c.name = '$sportName'"
-        );
-        $infoArray = $rawResponse->fetchAll(PDO::FETCH_ASSOC);  
-        $rawResponse->closeCursor();
-        return $infoArray;
-    }
-
+    );
+    $infoArray = $rawResponse->fetchAll(PDO::FETCH_ASSOC);
+    $rawResponse->closeCursor();
+    return $infoArray;
+}
