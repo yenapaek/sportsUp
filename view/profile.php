d863a01029cd0558b634ff54992a791d35f23712
Yena <yenapaek@gmail.com>
2021-05-10 15:29:53 +0900

    intital profile.css design

--- view/profile.php
+++ view/profile.php
@@ -6,26 +6,25 @@
 <div id="main-body">
     <section id="personnalInfo">
         <div class="info-container" id="myInfos">
-            <!-- <div> -->
-                <img src="./public/images/aboutUs/Alexis.png" alt="profile image" class="profile-img">
-            <!-- </div> -->
+            <!-- #TODO user must be able to upload an avatar when creating a profile -->
+            <div>
+                <img src="<?= !empty($infoProfile['avatar']) ? $infoProfile['avatar'] : 'http://cdn.onlinewebfonts.com/svg/img_258083.png'; ?>" alt="profile image" class="profile-img">
+            </div>
             <div>
                 <h1>Hi, <?= !empty($infoProfile) ? $infoProfile['userName'] : '...'; ?></h1>
             </div>
             <div>
-                <!-- <p>UserName : <?= !empty($infoProfile) ? $infoProfile['userName'] : '...'; ?></p> -->
                 <p>FirstName : <?= !empty($infoProfile['firstName']) ? $infoProfile['firstName'] : '...'; ?></p>
                 <p>LastName : <?= !empty($infoProfile['lastName']) ? $infoProfile['lastName'] : '...' ?></p>
                 <p>Birthday : <?= !empty($infoProfile['birthDate']) ? $infoProfile['birthDate'] : '...'; ?></p>
                 <p>Email : <?= !empty($infoProfile['email']) ? $infoProfile['email'] : '...' ?></p>
-                <p>Nationality : <?= !empty($infoProfile['nationality']) ? $infoProfile['nationality'] : '...' ?></p>
                 <p>City : <?= !empty($infoProfile['city']) ? $infoProfile['city'] : '...' ?></p>
             </div>
         </div>
 
         <div class="info-container" id="mySports">
             <div class="titleEdit">
-                <h1>My sport</h1>
+                <h1>My Interests</h1>
                 <button>Add Sport</button>
             </div>
             <div>
@@ -47,9 +46,9 @@
     <div id="changedInfo">
         <section id="eventInfo">
             <div class="info-container" id="myEvents">
-                <h1>My Events</h1>
+                <h1>Events</h1>
                 <div class="titleEdit">
-                    <h2>Created</h2>
+                    <h2>My Events</h2>
                     <button>Create Event</button>
                     <!-- INFO FROM DATABASE -->
                 </div>
@@ -72,17 +71,20 @@
                     </div>
                     <div class="event-card">
                         <img src="https://images.unsplash.com/photo-1607417307790-5f3efc48ced3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=749&q=80" alt="">
-                        <div>
+                        <div class="event-detail">
                             <div class="card-datetime">
                                 <i class="far fa-clock"></i>
                                 <span>Tues, May 11 @17:00</span>
                             </div>
-                            <div class="card-title"><span>Football Game in the Park</span></div>
-                            <div class="card-description"><span>Fun football game for all levels</span></div>
-                            <!-- <div class="card-location">                            
+                            <div>
+                                <i class="far fa-calendar"></i>
+                                <span class="card-title">Football Game in the Park</span>
+                            </div>
+                            <div class="card-location">                            
                                 <i class="fas fa-map-marker-alt"></i>
                                 <span>Seoul</span>
-                            </div> -->
+                            </div>
+                            <!-- <div class="card-description"><span>Fun football game for all levels</span></div> -->
                             <!-- <div class="card-btn"><span>View Details</span></div> -->
                         </div>
                     </div>
@@ -125,13 +127,14 @@
                         if (!empty($myEvents)) {
                             foreach ($myEvents as $event) {
                         ?>
-                                <li>
-                                    <?= 
+                                    <?php 
                                         #TODO create an event creater function that saves db data into an array
-                                        $event;
-                                    ?>
-                                </li><br>
+                                        // print_r($event);
+                                        // echo $event['name'];
+                                        // echo $event['picture'];
+                                        // echo $event['eventDate'];
 
+                                    ?>
                         <?php
                             }
                         } else {
